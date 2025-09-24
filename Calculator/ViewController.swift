96fd13396df18221931061853266460f9f31770a
iamadlet <adlet.zhumagaliev2003@gmail.com>
2025-09-25 03:37:58 +0500

    feat: Finished View, created plus and minus button actions

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var textField2: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func plus(_ sender: Any) {
+        let a = textField.text!
+        let b = textField2.text!
+        if let numberA = Int(a), let numberB = Int(b) {
+            let sum = numberA + numberB
+            label.text = String(sum)
+        } else {
+            label.text = "Введите 2 числа"
+        }
+    }
+    
+    @IBAction func minus(_ sender: Any) {
+        let a = textField.text!
+        let b = textField2.text!
+        if let numberA = Int(a), let numberB = Int(b) {
+            let sum = numberA - numberB
+            label.text = String(sum)
+        } else {
+            label.text = "Введите 2 числа"
+        }
+    }
+    
 }
 
